c06139284bfd6f4647d7df9ee83e46fd11a1bf33
SrikanthBommadi <bommadisrikanth1@gmail.com>
2025-07-24 10:40:00 -0400

    aws

--- Lamda/output.tf
+++
@@ -1,7 +0,0 @@
-output "instance_id" {
-  value = aws_instance.ec2.id
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
-}
--- Lamda/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "key_pair_name" {
-  description = "The name of the AWS EC2 key pair to use for SSH access"
-  type        = string
-}
